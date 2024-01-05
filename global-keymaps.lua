16aeaee3338599875c05c16f61f97b1abb62b6b6
Johan Yngman <johan.yngman@paradoxinteractive.com>
2024-01-05 14:53:48 +0100

    wip

--- global-keymaps.lua
+++ global-keymaps.lua
@@ -2,6 +2,7 @@
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>", { desc = "Toggle nvimtree" } )
+vim.keymap.set("n", "<Esc>", ":noh <CR>", { desc = "Clear highlights" } )
 
 -- Insert mode
 vim.keymap.set("i", "kj", "<esc>", { desc = "Exit insert mode" } )
