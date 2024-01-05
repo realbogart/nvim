c7e3859aababb90110f514f650b623256713dc1f
Johan Yngman <johan.yngman@paradoxinteractive.com>
2024-01-05 15:49:30 +0100

    wip

--- init.lua
+++ init.lua
@@ -219,32 +219,6 @@ require("which-key").register({
   ["<leader>h"] = { "Git [H]unk" },
 }, { mode = "v" })
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property "filetypes" to the map in question.
-local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { "html", "twig", "hbs"} },
-
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      -- NOTE: toggle below to ignore Lua_LS"s noisy `missing-fields` warnings
-      -- diagnostics = { disable = { "missing-fields" } },
-    },
-  },
-}
-
 -- Setup neovim lua configuration
 require("neodev").setup()
 
