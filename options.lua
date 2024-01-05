48b18d6b05288990a07cfa737d98d4d17f2e95df
Johan Yngman <johan.yngman@paradoxinteractive.com>
2024-01-05 13:10:47 +0100

    wip

---
+++ options.lua
@@ -0,0 +1,15 @@
+vim.wo.number = true
+vim.wo.signcolumn = "yes"
+
+vim.o.hlsearch = true
+vim.o.mouse = "a"
+vim.o.clipboard = "unnamedplus"
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = "menuone,noselect"
+vim.o.termguicolors = true
+
