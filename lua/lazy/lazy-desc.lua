dbcf95d5c03995940ca615c66e451dd4cc43163e
Johan Yngman <johan.yngman@gmail.com>
2024-01-05 22:48:56 +0100

    wip

--- lua/lazy/lazy-desc.lua
+++ lua/lazy/lazy-desc.lua
@@ -1,4 +1,20 @@
 return {
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "onedark"
+    end,
+  },
+
+  -- {
+  --   "drewtempelmeyer/palenight.vim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme "palenight"
+  --   end,
+  -- },
+  
   "tpope/vim-fugitive",
   "tpope/vim-rhubarb",
   "christoomey/vim-tmux-navigator",
@@ -7,18 +23,7 @@ return {
 
   require("lazy.harpoon"),
   require("lazy.telescope"),
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup {}
-    end,
-  },
+  require("lazy.nvim-tree"),
 
   {
     "neovim/nvim-lspconfig",
@@ -39,95 +44,6 @@ return {
     },
   },
 
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map({ "n", "v" }, "]c", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, desc = "Jump to next hunk" })
-
-        map({ "n", "v" }, "[c", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, desc = "Jump to previous hunk" })
-
-        -- Actions
-        -- visual mode
-        map("v", "<leader>hs", function()
-          gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
-        end, { desc = "stage git hunk" })
-        map("v", "<leader>hr", function()
-          gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
-        end, { desc = "reset git hunk" })
-        -- normal mode
-        map("n", "<leader>hs", gs.stage_hunk, { desc = "git stage hunk" })
-        map("n", "<leader>hr", gs.reset_hunk, { desc = "git reset hunk" })
-        map("n", "<leader>hS", gs.stage_buffer, { desc = "git Stage buffer" })
-        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
-        map("n", "<leader>hR", gs.reset_buffer, { desc = "git Reset buffer" })
-        map("n", "<leader>hp", gs.preview_hunk, { desc = "preview git hunk" })
-        map("n", "<leader>hb", function()
-          gs.blame_line { full = false }
-        end, { desc = "git blame line" })
-        map("n", "<leader>hd", gs.diffthis, { desc = "git diff against index" })
-        map("n", "<leader>hD", function()
-          gs.diffthis "~"
-        end, { desc = "git diff against last commit" })
-
-        -- Toggles
-        map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
-        map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
-
-        -- Text object
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "select git hunk" })
-      end,
-    },
-  },
-
-  -- {
-  --   "drewtempelmeyer/palenight.vim",
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme "palenight"
-  --   end,
-  -- },
-
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "onedark"
-    end,
-  },
-  
   {
     "nvim-lualine/lualine.nvim",
     opts = {
