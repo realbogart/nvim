088b3dce117806ef611368969bcfe6c91250ffe7
Johan Yngman <johan.yngman@gmail.com>
2024-01-06 11:20:19 +0100

    wip

--- lua/lazy/gitsigns.lua
+++ lua/lazy/gitsigns.lua
@@ -19,9 +19,9 @@ return {
       end
 
       -- Navigation
-      map({ "n", "v" }, "<leader>gn", function()
+      map({ "n", "v" }, "<leader>hn", function()
         if vim.wo.diff then
-          return "<leader>gn"
+          return "<leader>hn"
         end
         vim.schedule(function()
           gs.next_hunk()
@@ -29,9 +29,9 @@ return {
         return "<Ignore>"
       end, { expr = true, desc = "Jump to next hunk" })
 
-      map({ "n", "v" }, "<leader>gp", function()
+      map({ "n", "v" }, "<leader>hN", function()
         if vim.wo.diff then
-          return "<leader>gp"
+          return "<leader>hN"
         end
         vim.schedule(function()
           gs.prev_hunk()
@@ -43,6 +43,7 @@ return {
       map("v", "<leader>hr", function() gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end, { desc = "reset git hunk" })
       map("n", "<leader>hs", gs.stage_hunk, { desc = "git stage hunk" })
       map("n", "<leader>hr", gs.reset_hunk, { desc = "git reset hunk" })
+
       map("n", "<leader>hS", gs.stage_buffer, { desc = "git Stage buffer" })
       map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
       map("n", "<leader>hR", gs.reset_buffer, { desc = "git Reset buffer" })
@@ -50,8 +51,9 @@ return {
       map("n", "<leader>hb", function() gs.blame_line { full = false } end, { desc = "git blame line" })
       map("n", "<leader>hd", gs.diffthis, { desc = "git diff against index" })
       map("n", "<leader>hD", function() gs.diffthis "~" end, { desc = "git diff against last commit" })
-      map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
-      map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
+
+      -- map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
+      -- map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
 
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "select git hunk" })
     end,
