b5789dad3de95b3fb6a1a3a3489a48b886459fc0
Johan Yngman <johan.yngman@gmail.com>
2024-01-08 22:06:10 +0100

    wip

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup( require("lazy.lazy-desc"), {})
+require("lazy").setup( require("lazy-desc"), {})
 require("global-options")
 require("global-keymaps")
 
