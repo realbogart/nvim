ac13b95fe5f5cf40f88673db6b3299de05cda8c6
Johan Yngman <johan.yngman@paradoxinteractive.com>
2024-01-09 13:29:07 +0100

    Configured diffview.nvim

--- lua/lazy/diffview.lua
+++ lua/lazy/diffview.lua
@@ -1,4 +1,15 @@
 return {
-    "sindrets/diffview.nvim"
+  "sindrets/diffview.nvim",
+
+  config = function()
+    vim.keymap.set("n", "<leader>gd", 
+      function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end)
+  end,
 }
 
