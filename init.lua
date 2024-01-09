19aa416de3839e5ddf10c4b144d7bd690d71f7af
Johan Yngman <johan.yngman@paradoxinteractive.com>
2024-01-09 10:32:20 +0100

    wip

--- init.lua
+++ init.lua
@@ -13,6 +13,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- disable some default providers
+for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
+  vim.g["loaded_" .. provider .. "_provider"] = 0
+end
+
 require("lazy").setup( require("lazy-desc"), {})
 require("global-options")
 require("global-keymaps")
