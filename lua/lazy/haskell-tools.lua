26404e8985cce3531cec8879416f379e9e124288
Johan Yngman <johan.yngman@gmail.com>
2024-01-13 11:24:16 +0100

    Fixed haskell-tools configuration

--- lua/lazy/haskell-tools.lua
+++ lua/lazy/haskell-tools.lua
@@ -7,13 +7,15 @@ return {
     },
     branch = '2.x.x',
     ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
-    config = function()
-        local ht = require('haskell-tools')
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { noremap = true, silent = true, buffer = bufnr, }
-        -- haskell-language-server relies heavily on codeLenses,
-        -- so auto-refresh (see advanced configuration) is enabled by default
-        vim.keymap.set('n', '<leader>ca', vim.lsp.codelens.run, opts)
-        vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, opts)
+    config = function ()
+        vim.g.haskell_tools = {
+            hls = {
+                on_attach = function(client, bufnr, ht)
+                    local opts = { noremap = true, silent = true, buffer = bufnr, }
+                    vim.keymap.set('n', '<leader>ca', vim.lsp.codelens.run, opts)
+                    vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, opts)
+                end,
+            }
+        }
     end,
 }
