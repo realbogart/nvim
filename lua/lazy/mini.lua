ab901da67e6759b5748e6cc7e4a8ea2993143a73
Johan Yngman <johan.yngman@gmail.com>
2024-06-11 14:51:43 +0000

    miniai config

--- lua/lazy/mini.lua
+++ lua/lazy/mini.lua
@@ -3,6 +3,22 @@ return {
 	version = "*",
 
 	config = function()
-		require("mini.ai").setup()
+		local opts = {
+			custom_textobjects = nil,
+			mappings = {
+				around = "a",
+				inside = "i",
+				around_next = "an",
+				inside_next = "in",
+				around_last = "al",
+				inside_last = "il",
+				goto_left = "]a",
+				goto_right = "[a",
+			},
+			n_lines = 50,
+			search_method = "cover_or_next",
+			silent = false,
+		}
+		require("mini.ai").setup(opts)
 	end,
 }
