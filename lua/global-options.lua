034217a50b6fcde1243df9beca0e9d914a2276a4
Johan Yngman <johan.yngman@gmail.com>
2024-08-15 11:33:37 +0000

    Blinking insert cursor

--- lua/global-options.lua
+++ lua/global-options.lua
@@ -24,3 +24,9 @@ vim.opt.expandtab = true
 vim.diagnostic.config({
 	virtual_text = false,
 })
+
+vim.o.guicursor = table.concat({
+	"n-v-c:block-Cursor",
+	"i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
+}, ",")
