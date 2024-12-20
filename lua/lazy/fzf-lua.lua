f093dfc2145b0f79905410aa47731fbdd586fa87
Johan Yngman <johan.yngman@gmail.com>
2024-12-20 14:30:58 +0000

    fzf-lua config

--- lua/lazy/fzf-lua.lua
+++ lua/lazy/fzf-lua.lua
@@ -6,15 +6,38 @@ return {
 		},
 	},
 	config = function()
+		local actions = require("fzf-lua.actions")
 		local opts = {
 			grep = {
 				rg_glob = true,
 			},
+			keymap = {
+				fzf = {
+					-- ["ctrl-f"] = "abort",
+					["ctrl-d"] = "half-page-down",
+					["ctrl-u"] = "half-page-up",
+				},
+			},
+			fzf_opts = {
+				["--layout"] = "reverse-list",
+			},
+			winopts = {
+				fullscreen = true,
+				preview = {
+					layout = "vertical",
+					vertical = "up:60%",
+				},
+			},
+			actions = {
+				files = {
+					["enter"] = actions.file_edit_or_qf,
+				},
+			},
 		}
 		require("fzf-lua").setup(opts)
 
-		vim.keymap.set("n", "<C-f>", "<cmd> FzfLua live_grep_glob <CR>", { desc = "Live grep" })
-		vim.keymap.set("i", "<C-f>", "<cmd> FzfLua live_grep_glob <CR>", { desc = "Live grep" })
+		vim.keymap.set("n", "<C-f>", "<cmd> FzfLua live_grep_resume <CR>", { desc = "Live grep" })
+		vim.keymap.set("i", "<C-f>", "<cmd> FzfLua live_grep_resume <CR>", { desc = "Live grep" })
 		vim.keymap.set("v", "<C-f>", "<cmd> FzfLua grep_visual <CR>", { desc = "Grep word under cursor" })
 	end,
 }
