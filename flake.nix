3cfd29820265e7306fa0bbe1fa11b02e326a942e
Johan Yngman <johan.yngman@gmail.com>
2025-06-19 11:48:45 +0000

    Test

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   description = "My neovim setup";
 
+  inputs.self.submodules = true;
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    self.submodules = true;
   };
 
   outputs = { self, nixpkgs, flake-utils }:
